cf71eae262084a70c99a8029275e329fffbb3454
Kevin21 <kevu4776@gmail.com>
2025-03-03 15:26:18 -0600

    created a factory function for the player object as well as a gameboard IIFE that contains the necessary functionality for it

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function player(){
+const player = (name, symbol) => {
+    return{ name, symbol };
+};
 
-}
+const gameBoard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""]
 
+    const getBoard = () => [...board];
+    const resetBoard = () => board.fill("");
+    const fillSpot = (index, symbol) => {
+        board[index] == "" ? board[index] = symbol : alert("This spot has already been taken!!!");
+    };
+    const isFull = () => board.every(cell => cell != "");
+
+    return { getBoard, resetBoard, fillSpot, isFull};
+})();
